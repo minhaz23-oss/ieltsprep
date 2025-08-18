d53adec3c5df5e715a1af2704672f2677a03c508
Minhaz Fardin <minhazfardin25@gmail.com>
2025-08-19 05:50:58 +0600

    n3

--- types/index.d.ts
+++ types/index.d.ts
@@ -2,3 +2,19 @@ interface Window {
   SpeechRecognition: any;
   webkitSpeechRecognition: any;
 }
+
+// Auth form types
+type FormType = 'sign-in' | 'sign-up';
+
+// Auth action parameter types
+interface SignUpParams {
+  uid: string;
+  email: string;
+  password: string;
+  name: string;
+}
+
+interface SignInParams {
+  email: string;
+  idToken: string;
+}
